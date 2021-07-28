13a872ae5759d9f092562d6da8c06332244d5200
Ankush Naudiyal <67361935+xJOKERxxx@users.noreply.github.com>
2021-07-28 15:05:43 +0530

    Add files via upload

---
+++ L2-D2/Basic of ES6/forofLoop/index.js
@@ -0,0 +1,9 @@
+const students = [
+    { name: "Ankush", city: "Dehradun" },
+    { name: "Arpit", city: "Dehli" },
+    { name: "Nick", city: "Mumbai" }
+]
+
+for (const student of students) {
+    console.log(`${student.name} lives in ${student.city}`);
+}
