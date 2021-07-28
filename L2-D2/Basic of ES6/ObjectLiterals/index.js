dcc361b02279b85faa547b6f98628d187cbbf706
Ankush Naudiyal <67361935+xJOKERxxx@users.noreply.github.com>
2021-07-28 15:08:41 +0530

    Add files via upload

---
+++ L2-D2/Basic of ES6/ObjectLiterals/index.js
@@ -0,0 +1,6 @@
+function adressMaker(city, state) {
+    //const newAdress={newCity:city ,newState:state};
+    const newAdress = { city, state }; //in object literals we dont have write duplicate code.
+    console.log(newAdress);
+}
+adressMaker('Dehradun', 'UK');
